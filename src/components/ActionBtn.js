87f3988223ef9c12b6456684a1409ee28d4df200
devKuldip <devkuldip836@gmail.com>
2023-01-10 01:28:57 +0530

    updated

--- src/components/ActionBtn.js
+++ src/components/ActionBtn.js
@@ -1,9 +1,5 @@
-import React, { useContext } from 'react'
-import CartContext from "hooks/CartContext";
+import React from 'react'
 function ActionBtn({addToCart, prodId}) {
-    // const cart = useContext(CartContext)
-    // const items = cart.products;
-    //   const isInCart = prodId && items.hasOwnProperty(`${prodId}`);
   return (
     <button type="button" className='primary_btn' onClick={()=>addToCart(prodId)} >Add To Cart</button>
   )
